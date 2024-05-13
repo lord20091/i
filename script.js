413302bf97fc979328bc9c32b1c90c74ae13be4a
lord20091 <abdulazizbozorov009@gmail.com>
2024-05-13 16:57:15 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let name = prompt("Ismingizni kiriting:");
+let spaceId = prompt("Yoshingizni kiriting:");
+let spacePassword = prompt("Manzilizni kiriting:");
+
+let spaceObject = {
+  name: name,
+  age: spaceId,
+  center: spacePassword
+};
+
+console.log(spaceObject)
